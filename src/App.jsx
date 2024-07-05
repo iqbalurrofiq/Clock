f24b71e245cbd720ecb895b3c1826c6a388d88ed
iqbalurrofiq <iqbalurrofiq17@gmail.com>
2024-07-05 23:44:42 +0700

    menambahkan react navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+// In App.js in a new project
 
-const App = () => {
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
-
-const styles = StyleSheet.create({});
