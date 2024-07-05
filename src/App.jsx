305edfa3caf7371ea2ddca89732fa51ccd29179a
iqbalurrofiq <iqbalurrofiq17@gmail.com>
2024-07-06 01:33:32 +0700

    menambah bottom tabs pada app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-// In App.js in a new project
-
 import * as React from 'react';
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 function HomeScreen() {
   return (
@@ -13,14 +11,50 @@ function HomeScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+function AlarmScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Alarm Screen</Text>
+    </View>
+  );
+}
+
+function WorldClockScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>World Clock Screen</Text>
+    </View>
+  );
+}
+
+function TimerScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Timer Screen</Text>
+    </View>
+  );
+}
+
+function StopwatchScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Stopwatch Screen</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Alarm" component={AlarmScreen} />
+        <Tab.Screen name="World Clock" component={WorldClockScreen} />
+        <Tab.Screen name="Timer" component={TimerScreen} />
+        <Tab.Screen name="Stopwatch" component={StopwatchScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
