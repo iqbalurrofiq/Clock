985e0c3730d2ec731a7e605ee28b0c6408e472b5
iqbalurrofiq <iqbalurrofiq17@gmail.com>
2024-07-06 17:47:55 +0700

    menambah vector icon

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,49 @@ import AlarmScreen from './screens/AlarmScreen';
 import WorldClockScreen from './screens/WorldClockScreen';
 import TimerScreen from './screens/TimerScreen';
 import StopwatchScreen from './screens/StopwatchScreen';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'Alarm':
+                iconName = 'alarm';
+                break;
+              case 'World Clock':
+                iconName = 'earth';
+                break;
+              case 'Timer':
+                iconName = 'timer';
+                break;
+              case 'Stopwatch':
+                iconName = 'stopwatch';
+                break;
+              default:
+                iconName = 'help-circle';
+                break;
+            }
+
+            // Return the icon component
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                color={color}
+                size={size}
+              />
+            );
+          },
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Alarm" component={AlarmScreen} />
         <Tab.Screen name="World Clock" component={WorldClockScreen} />
